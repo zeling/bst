cf63280e8d80739aa834677b066897ae19afa35e
zeling <zeling.feng@hotmail.com>
2018-05-30 12:24:30 +0800

    added main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "skiplist.h"
 
 int main() {
+    skiplist<int, 4> sklist;
+    sklist.insert(0);
+    sklist.find(0);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
